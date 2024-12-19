43cec84bb3b152422b01512f38600c26c4f2871c
Raphdf201 <deschenesr545@gmail.com>
2024-12-19 18:09:52 -0500

    Added dirt_pick item, model and texture

--- src/main/java/net/raphdf201/oppickaxes/Items.java
+++ src/main/java/net/raphdf201/oppickaxes/Items.java
@@ -1,18 +1,26 @@
 package net.raphdf201.oppickaxes;
 
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.PickaxeItem;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
+import net.raphdf201.oppickaxes.materials.Tools;
 
 import static net.raphdf201.oppickaxes.OPPickaxes.MOD_ID;
 
 public class Items {
+    public static final Item DIRT_PICK = register(new PickaxeItem(Tools.dirt, 0, Integer.MAX_VALUE, new Item.Settings()), "dirt_pick");
+
     public static Item register(Item item, String id) {
         // Create the identifier for the item.
         Identifier itemID = Identifier.of(MOD_ID, id);
 
         // Register and return the item!
         return Registry.register(Registries.ITEM, itemID, item);
+    }
+
+    public static void initialize() {
     }
 }
--- src/main/java/net/raphdf201/oppickaxes/materials/Tools.java
+++ src/main/java/net/raphdf201/oppickaxes/materials/Tools.java
@@ -5,5 +5,12 @@ import net.minecraft.registry.tag.BlockTags;
 import net.minecraft.registry.tag.ItemTags;
 
 public class Tools {
-    public static final ToolMaterial dirt = new ToolMaterial(BlockTags.INCORRECT_FOR_NETHERITE_TOOL, 999, 999, 0, 0, ItemTags.DIRT);
+    public static final ToolMaterial dirt = new ToolMaterial(
+            BlockTags.INCORRECT_FOR_NETHERITE_TOOL, // Block breaking capabilities
+            Integer.MAX_VALUE,      // Durability
+            Integer.MAX_VALUE,      // Speed
+            0,                      // Attack damage
+            0,                      // enchantmentValue
+            ItemTags.DIRT           // Repair items
+    );
 }

e81637df480bb321c76db8f17112dfa8080b4837
raphdf201 <raphdf201@hotmail.com>
2025-06-30 10:20:18 -0400

    Init architectury

---
+++ common/src/main/java/net/raphdf201/oppickaxes/Oppickaxes.java
@@ -0,0 +1,9 @@
+package net.raphdf201.oppickaxes;
+
+public final class Oppickaxes {
+    public static final String MOD_ID = "oppickaxes";
+
+    public static void init() {
+        // Write common init code here.
+    }
+}
